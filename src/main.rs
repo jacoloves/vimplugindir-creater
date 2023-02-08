fbcf25d79f5119b1e204e1d22e786de6f025de85
jacoloves <5511068t@gmail.com>
2023-02-08 23:19:49 +0900

    20230208

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,64 @@
 use std::env;
+use std::fs;
 
 extern crate dirs;
 
+static AUTOLOAD: &str = "autoload";
+static DOC: &str = "doc";
+static PLUGIN: &str = "plugin";
+static VIMPLUGINDIR: &str = "VIMPLUGINDIR";
+static NVIMPLUGINDIR: &str = "NVIMPLUGINDIR";
+
 fn main() {
-    //println!("{}", replace_dir_string("/home/stanaka/test".to_string()));
+    let args: Vec<String> = env::args().collect();
+    let plugin_name: String = args[1].clone();
+
+    execute(plugin_name);
 }
 
-pub fn execute() {
+pub fn execute(plugin_name: String) {
     let vim_plugin_dir: String = read_vimplugindir_var();
     let nvim_plugin_dir: String = read_nvimplugindir_var();
 
     if dir_variable_check(&vim_plugin_dir, &nvim_plugin_dir) {
-        let rep_vim_plugin_dir = replace_dir_string(vim_plugin_dir);
+        let vim_plugin_dir = replace_dir_string(vim_plugin_dir);
+        let nvim_plugin_dir = replace_dir_string(nvim_plugin_dir);
+
+        /* vim plugin dir create */
+        if !(vim_plugin_dir.is_empty()) {
+            let root_plugin_dir = create_plugin_dir_name(vim_plugin_dir, plugin_name);
+            let judge_flg: bool = match fs::create_dir(root_plugin_dir.clone()) {
+                Ok(_) => true,
+                Err(_) => false,
+            };
+
+            if judge_flg {
+                println!("{} is created complete", root_plugin_dir.clone());
+            } else {
+                eprintln!("{} is not created", root_plugin_dir);
+            }
+        }
     }
 }
 
 pub fn read_vimplugindir_var() -> String {
     let mut dir: String = String::new();
-    if match env::var("VIMPLUGINDIR") {
+    if match env::var(VIMPLUGINDIR) {
         Ok(_) => true,
         Err(_) => false,
     } {
-        dir = env::var("VIMPLUGINDIR").unwrap();
+        dir = env::var(VIMPLUGINDIR).unwrap();
     }
     dir
 }
 
 pub fn read_nvimplugindir_var() -> String {
     let mut dir: String = String::new();
-    if match env::var("NVIMPLUGINDIR") {
+    if match env::var(NVIMPLUGINDIR) {
         Ok(_) => true,
         Err(_) => false,
     } {
-        dir = env::var("NVIMPLUGINDIR").unwrap();
+        dir = env::var(NVIMPLUGINDIR).unwrap();
     }
     dir
 }
@@ -59,8 +85,21 @@ pub fn replace_dir_string(dirstring: String) -> String {
     dir
 }
 
+pub fn create_plugin_dir_name(dir: String, plugin_name: String) -> String {
+    let plugin_dir: String = dir + &"/".to_string() + &plugin_name;
+    plugin_dir
+}
+
+pub fn create_plugin_detail_dir(dir: String) {
+    let autoload_dir: String = create_plugin_dir_name(dir.clone(), AUTOLOAD.to_string());
+    let doc_dir: String = create_plugin_dir_name(dir.clone(), DOC.to_string());
+    let plugin_dir: String = create_plugin_dir_name(dir.clone(), PLUGIN.to_string());
+}
+
 #[cfg(test)]
 mod test {
+    use std::{fs::create_dir, path::Path};
+
     use super::*;
 
     #[test]
@@ -94,6 +133,24 @@ mod test {
             replace_dir_string("/home/stanaka/test".to_string()),
             "/home/stanaka/test"
         );
+    }
+
+    #[test]
+    fn test_create_plugin_dir_name() {
+        assert_eq!(
+            create_plugin_dir_name("/home/stanaka/test".to_string(), "test-tool".to_string()),
+            "/home/stanaka/test/test-tool"
+        );
+    }
+
+    #[test]
+    fn test_create_plugin_detail_dir() {
+        let vim_plugin_dir: String = read_vimplugindir_var();
+        let vim_plugin_dir = replace_dir_string(vim_plugin_dir);
+        let root_plugin_dir = create_plugin_dir_name(vim_plugin_dir, "test-tool".to_string());
+        fs::create_dir(root_plugin_dir.clone()).unwrap();
+
+        create_plugin_detail_dir(root_plugin_dir)
     }
 
     #[test]
